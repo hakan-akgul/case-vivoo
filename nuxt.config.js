63ae79f64e47b2d443c8c11095584a834ef36e28
hakan akgül <mail.hakanakgul@gmail.com>
2022-09-11 20:12:36 +0300

    chore: :wrench: font and colors configs added

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,13 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" }
     ]
   },
 
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'yankees-blue': '#181a37',
+        'pearl-aqua': '#74c6b8',
+        'cultured': '#f4f6fa',
+        'mango-tango': '#ff7a45',
+        'pastel-red': '#ff6264',
+        'arsenic': '#3e424b',
+        'cool-black': '#07255b',
+        'auro-metal-saurus': '#66737f'
+      },
+      fontFamily: {
+        'poppins': ['Poppins']
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
