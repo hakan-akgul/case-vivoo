39daf4628f68bc46c44959bddd3e000aa67147cb
hakan akgül <mail.hakanakgul@gmail.com>
2022-09-12 23:02:23 +0300

    feat: :wrench: tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -11,10 +12,28 @@ module.exports = {
         'pastel-red': '#ff6264',
         'arsenic': '#3e424b',
         'cool-black': '#07255b',
-        'auro-metal-saurus': '#66737f'
+        'auro-metal-saurus': '#66737f',
+        'detail-1': '#023E96',
+        'detail-2': '#ED427D',
+        'detail-3': '#FE8364',
+        'detail-4': '#112C56',
+
       },
       fontFamily: {
         'poppins': ['Poppins']
+      },
+      spacing: {
+        '1.25': '5px',
+        '1.5': '6px',
+        '1.75': '7px',
+        '2.25': '10px',
+        '3.5': '14px',
+        '13': '52px',
+        '128': '512px',
+        '160': '640px',
+        '196': '768px',
+        '256': '1024px',
+        '400': '1200px'
       }
     }
   },
