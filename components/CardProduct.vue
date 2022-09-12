f024fbc56521c09c096311a8c491c5ee85777ba9
hakan akgül <mail.hakanakgul@gmail.com>
2022-09-12 23:08:00 +0300

    refactor: :lipstick: product cart styling improved

--- components/CardProduct.vue
+++ components/CardProduct.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="relative">
     <div
-      class="absolute w-8 h-8 p-2 bg-white rounded-full right-4 top-4"
+      class="absolute w-8 h-8 pt-2 pl-1.75 bg-white rounded-full right-4 top-4"
       @click="favorite = !favorite"
     >
       <img v-if="favorite" src="assets/heart-fill.svg" alt="heart-fill" />
       <img v-if="!favorite" src="assets/heart-empty.svg" alt="heart-empty" />
     </div>
+
     <div
       @click="goToDetail()"
-      class="flex flex-col items-center p-4 m-2 overflow-hidden font-semibold  w-44 h-52 rounded-2xl bg-cultured hover:bg-indigo-100 font-poppins"
+      class="flex flex-col items-center p-4 overflow-hidden font-semibold  w-44 h-52 rounded-2xl bg-cultured hover:bg-indigo-100 font-poppins"
     >
       <div
         class="w-full h-20 mt-8 mb-4 bg-center bg-no-repeat bg-contain"
         :style="{ backgroundImage: 'url(' + product.image + ')' }"
       />
+
       <p class="w-full h-8 overflow-hidden text-sm truncate text-yankees-blue">
         {{ product.title }}
       </p>
+
       <p class="w-full h-8 text-xs text-arsenic">${{ product.price }}</p>
     </div>
   </div>
