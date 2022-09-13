1e15309296919262a913d373ad2881cb292fb538
hakan akgül <mail.hakanakgul@gmail.com>
2022-09-13 12:54:15 +0300

    feat: :sparkles: cart completed

--- pages/Cart.vue
+++ pages/Cart.vue
@@ -1,16 +1,57 @@
 <template>
-  <div id="cart" class="p-4">
+  <div id="cart" class="relative h-screen p-4 pt-24">
+    <div
+      id="cart-buttons"
+      class="absolute flex justify-between w-full pr-8 top-12"
+    >
+      <nuxt-link to="ProductDetail">
+        <div class="w-12 h-12 pt-4 pl-4 rounded-full bg-cultured">
+          <img src="assets/back.svg" alt="back" />
+        </div>
+      </nuxt-link>
+      <nuxt-link to="/">
+        <div class="w-12 h-12 pt-3 pl-2.25 rounded-full bg-cultured">
+          <img src="assets/cancel.svg" alt="cart" />
+        </div>
+      </nuxt-link>
+    </div>
+
+    <p class="mb-4 text-2xl font-bold text-center text-yankees-blue">Cart</p>
+
     <CardCart
       v-for="(item, id, index) in cart"
       :key="index"
       :itemId="id"
       @updateTotalPrice="updateTotalPrice()"
     />
+
     <div>
-      <p>Total</p>
-      <p>{{ totalPrice }}</p>
+      <p class="font-medium">Enter Promo Code</p>
+      <div class="w-full p-4 my-4 bg-cultured rounded-xl">
+        <input
+          type="text"
+          placeholder="Enter your promo code"
+          class="w-full appearance-none bg-cultured focus-visible:outline-none"
+        />
+      </div>
+      <button
+        class="w-full mt-4 border-2  text-yankees-blue border-yankees-blue rounded-xl h-14"
+      >
+        Apply
+      </button>
     </div>
-    <button class="text-white bg-yankees-blue rounded-xl">Checkout</button>
+
+    <div class="absolute w-full pr-8 bottom-4">
+      <div class="flex items-center justify-between">
+        <p class="text-arsenic">Total</p>
+        <p class="text-lg font-semibold justify-self-end text-yankees-blue">
+          ${{ totalPrice }}
+        </p>
+      </div>
+      <button class="w-full mt-4 text-white bg-yankees-blue rounded-xl h-14">
+        Checkout
+      </button>
+    </div>
   </div>
 </template>
 
